9ade48f6dfc0b100ef3bebd0fbb1280f93bd7ea6
thecsw <thecsmoothiew@yahoo.com>
2017-10-25 22:35:31 +0400

    More comments and  TODO list

--- mser.py
+++ mser.py
@@ -1,3 +1,6 @@
+#TODO:
+#1. Make a variable to store the musics' destination/folder/directory
+#2. Get rid of the mutagen, not needed. Became redundant
 import os
 import sys
 import time
@@ -5,7 +8,6 @@ import time
 from mutagen.mp3 import MP3
 import telepot
 from telepot.loop import MessageLoop
-
 #Telegram bot key
 key = ''
 bot = telepot.Bot(key)
@@ -15,30 +17,31 @@ q = []
 empty = False
 #The varibale to hold listing
 a = os.listdir('Music')
+#THis one will hold the archive, that will be outputed
 listall = """Music archive."""
-
+#List all music in the directory
 for i in range(0, len(a)):
     audio = MP3('Music/{}'.format(a[i]))
     length = audio.info.length
     print('{}.{}'.format(i, a[i]))
     print('Length - {} secs'.format(length))
     listall = listall + '\n{}.{}'.format(i, a[i])
-    
+# Send message to user with their user ID
 def sms(ID, str):
         bot.sendMessage(ID, str)
-    
+# Reply message to user with their user ID and to the message ID    
 def reply(ID, msgID, str):
         bot.sendMessage(ID, str, None, None, None, msgID)
-    
+# Handling all new messages, like the getUpdates()
 def handle(msg):
     user_id = msg['chat']['id']
     msg_id = msg['message_id']
     command = msg['text'].encode('utf-8')
-    
+    #Output the whole directory
     if command == '/help':
         sms(user_id, listall)
         return
-    
+    #Insert from the tail
     q.insert(0, command)
     print('Inserted new song - {}'.format(a[int(command)]))
     print('Total queue size - {}'.format(len(q)))
@@ -47,8 +50,9 @@ Your place in the queue - {}'.format(
     a[int(command)],
     len(q)
     ))
-    
+#Checking for new messages
 MessageLoop(bot, handle).run_as_thread()
+#Now if the queue is bigger than 0, then pop the 0 element and start playing, repeat
 while 1:
     if (len(q)>0):
         try:
@@ -64,6 +68,7 @@ while 1:
         except:
             print('ERROR')
     else:
+        #if it's empty
         if empty == False:
             print('Queue is empty. Nothing to play')
             empty = True
